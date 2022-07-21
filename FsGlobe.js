6717c76157e2a00c5688a769038303d70bfb273d
mburlinson <mburlinson@ifmm.com>
2022-07-21 14:56:57 -0400

    v13

--- FsGlobe.js
+++ FsGlobe.js
@@ -40,7 +40,7 @@ function FsGlobe() {
   controls.minDistance = 1.2;
   controls.maxDistance = 4;
   controls.autoRotate = true;
-  controls.autoRotateSpeed = 0.15;
+  controls.autoRotateSpeed = 0.1;
   // //  controls.enableDamping = true;
   //   controls.campingFactor = 0.25;
   controls.enableZoom = false;
